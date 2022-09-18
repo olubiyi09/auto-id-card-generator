ff55772ffd85f4d8f7e385aa62ca4339954ce7e8
olubiyi09 <olubiyi09@gmail.com>
2022-09-18 21:27:45 +0400

    set up the enviroment, wrote the markup and style it

---
+++ script.js
@@ -0,0 +1,5 @@
+const stName = document.querySelector(".card-name");
+const regNo = document.querySelector(".card-reg");
+const gender = document.querySelector(".card-gender");
+const dept = document.querySelector(".card-dept");
+const img = document.querySelector(".id-img");
